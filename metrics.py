f1150eb63d09c3bc235cc7aafdba00f7babe92e9
MariaLukina <maria.v.luk@gmail.com>
2024-02-12 16:48:45 +0700

    Add knn.py and metrics.py with part of the functions

---
+++ metrics.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+def binary_classification_metrics(y_pred, y_true):
+    """
+    Computes metrics for binary classification
+    Arguments:
+    y_pred, np array (num_samples) - model predictions
+    y_true, np array (num_samples) - true labels
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    """
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+
+    """
+    YOUR CODE IS HERE
+    """
+    pass
+
+
+def multiclass_accuracy(y_pred, y_true):
+    """
+    Computes metrics for multiclass classification
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true labels
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    """
+
+    """
+    YOUR CODE IS HERE
+    """
+    pass
+
+
+def r_squared(y_pred, y_true):
+    """
+    Computes r-squared for regression
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true values
+    Returns:
+    r2 - r-squared value
+    """
+    y_mean = np.mean(y_true)
+    return  1 - ((np.square(np.subtract(y_pred, y_true))).sum())/np.square(y_true - y_mean).sum()
+
+
+
+def mse(y_pred, y_true):
+    """
+    Computes mean squared error
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true values
+    Returns:
+    mse - mean squared error
+    """
+    #return np.mean((y_pred - y_true)**2)
+    return np.square(y_pred - y_true).mean()
+
+
+def mae(y_pred, y_true):
+    """
+    Computes mean absolut error
+    Arguments:
+    y_pred, np array of int (num_samples) - model predictions
+    y_true, np array of int (num_samples) - true values
+    Returns:
+    mae - mean absolut error
+    """     
+    return np.absolute(np.subtract(y_pred, y_true)).mean() 
+
+    
